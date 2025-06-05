bd4e7226385918708fd55bb09ff1e7ef5b491e44
Sena <dimatesebas73@gmail.com>
2025-06-05 16:56:35 -0500

    x

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: "https://quindi-shoes-frontend-yemj.vercel.app", // Cambia esto a tu frontend
+    origin: "http://localhost:5173", // Cambia esto a tu frontend
     credentials: true,
     exposedHeaders: ["x-renewed-token"], 
   }));
