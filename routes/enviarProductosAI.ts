94bef337a107806d58d9054fcb5ebcec5e13a484
Sena <dimatesebas73@gmail.com>
2025-06-05 17:26:07 -0500

    mejora de rutas

--- routes/enviarProductosAI.ts
+++ routes/enviarProductosAI.ts
@@ -4,6 +4,6 @@ import enviarProductosAI from "../controllers/enviarProductos-ai-controller";
 const router = express.Router();
 
 // Se protege la ruta con los middlewares de verificación de token, si fuera necesario
-router.post("/", enviarProductosAI);
+router.post("/process-products", enviarProductosAI);
 
 export default router;
