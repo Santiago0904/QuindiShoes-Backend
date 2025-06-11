8a62037bd49968ae2b3c15f4b79722b1c938a8f1
Santiago Bustos Diaz <santiago.bustos0409@gmail.com>
2025-06-11 16:38:53 -0500

    Se creo la funcion para activar o desactivar la funcion de la reserva en un producto en concreto

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: "https://quindi-shoes-frontend-yemj.vercel.app", 
+    origin: "http://localhost:5173", 
     credentials: true,
     exposedHeaders: ["x-renewed-token"], 
   }));
