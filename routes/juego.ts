cc5b417c647a12595c47da10bd97ef7d62b4e0de
CamiloDuranMesa <duranmesacamilo@gmail.com>
2025-07-07 14:48:06 -0500

    x

--- routes/juego.ts
+++ routes/juego.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { guardarPuntuacion, obtenerTops } from "../controllers/Juego-controller";
 import { verifyToken } from "../controllers/renovar-token-controller";
+import { obtenerEstadoDescuentoController } from "../controllers/usuario-controller";
 
 const router = Router();
 
 router.post("/", verifyToken, guardarPuntuacion);
 
 router.get("/", obtenerTops);
+
+router.get('/:id/descuento-estado', obtenerEstadoDescuentoController);
 
 export default router;
--- routes/usuario.ts
+++ routes/usuario.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { obtenerHistorialCompras } from "../controllers/historial-compras-controller";
 import { obtenerUsuarioPorId } from "../controllers/usuario-controller";
-import { obtenerEstadoDescuentoController } from "../controllers/usuario-controller";
+
 const router = express.Router();
 
 router.get("/:id", obtenerUsuarioPorId);
 router.get("/:id_usuario/historial-compras", obtenerHistorialCompras);
-router.get('/:id/descuento-estado', obtenerEstadoDescuentoController);
+
 
 export default router;
