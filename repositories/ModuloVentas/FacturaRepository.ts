aaa7f5303f0c21c5899751de627231c94d6d3a6e
santiagoAguirreCastano <santiagoaguirrecastano8@gmail.com>
2025-07-10 14:17:14 -0500

    Merge branch 'main' into develop

--- repositories/ModuloVentas/FacturaRepository.ts
+++ repositories/ModuloVentas/FacturaRepository.ts
@@ -3,14 +3,28 @@ import db from '../../config/config-db'
 
 export default class FacturaRepository {
   static async obtenerFacturasDomicilio() {
-    const sql = `SELECT * FROM facturas WHERE metodo_entrega = 'domicilio' ORDER BY id DESC`;
-    const [rows]: any = await db.query(sql);
+  const sql = `
+    SELECT 
+      f.*, 
+      u.nombre, 
+      u.apellido, 
+      u.direccion, 
+      u.telefono, 
+      u.correo
+    FROM facturas f
+    JOIN users u ON f.id_usuario = u.id_usuario
+    WHERE f.metodo_entrega = 'domicilio'
+    ORDER BY f.id DESC
+  `;
 
-    return rows.map((factura: any) => ({
-      ...factura,
-      contenido_factura: typeof factura.contenido_factura === 'string'
-        ? JSON.parse(factura.contenido_factura)
-        : factura.contenido_factura,
-    }));
-  }
+  const [rows]: any = await db.query(sql);
+
+  return rows.map((factura: any) => ({
+    ...factura,
+    contenido_factura: typeof factura.contenido_factura === 'string'
+      ? JSON.parse(factura.contenido_factura)
+      : factura.contenido_factura,
+  }));
+}
+
 }
